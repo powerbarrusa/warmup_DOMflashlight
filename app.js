aee93f101e9ff2ccec536f7143bce6a1b4d79d6e
Barr Covrigaru <barr@covrigaru.com>
2018-11-21 10:15:56 -0700

    No pushes in a while. Needed to 'save'

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-// Your code goes here
-
+  var light = document.getElementsByClassName("off");
+  var mouseClick = function() {
+    // var lightDiv = document.createElement("div");
+        light.className = "on";
+        document.body.appendChild(light);        
+    };
+    // 3. Add the event listener for the element and function
+    light.addEventListener("click", mouseClick);
 })
