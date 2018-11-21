bf6719ccd701260b19db1dbffc2e590d97f47fdb
Barr Covrigaru <barr@covrigaru.com>
2018-11-21 10:32:20 -0700

    the light turns on woot woot

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  var light = document.getElementsByClassName("off");
+  var light = document.getElementById("flashlight");
   var mouseClick = function() {
-    // var lightDiv = document.createElement("div");
-        light.className = "on";
-        document.body.appendChild(light);        
+      light.classList.toggle("on");
+      light.classList.toggle("off");      
     };
     // 3. Add the event listener for the element and function
     light.addEventListener("click", mouseClick);
